dd05c927c70f872fd999b10d531748afead6888c
DawnlightForest <rn.aite0820@gmail.com>
2025-07-08 13:03:14 -0700

    added an image

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import theImage from './cat.avif'; 
 
-function App() {
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <img src={logo} className="App-logo" alt="logo" />
-       
-       <p>this is my app!!!</p>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>This is my app!!!</p>
+        </header>
+      </div>
+
+      <div className="image_container">
+        <img src={theImage} />
+        <p>This is a beautiful image from the internet!</p>
+      </div>
+    </>
   );
 }
 
